1efefac10e7d291db2fed133fed27f917d32e802
shrya25 <126152407+shrya25@users.noreply.github.com>
2023-03-23 20:47:18 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+# import the model
+pipe = pickle.load(open('expipe.pkl','rb'))
+df = pickle.load(open('exdf.pkl','rb'))
+
+st.title("Laptop Predictor")
+
+# brand
+company = st.selectbox('Brand',data['Brand'].unique())
+
+# type of laptop
+type = st.selectbox('Type',data['type'].unique())
+
+# Ram
+ram = st.selectbox('RAM(in GB)',[2,4,6,8,12,16,24,32,64])
+
+# screen size
+screen_size = st.number_input('Screen Size')
+
+#cpu
+cpu = st.selectbox('CPU',data['cpu brand'].unique())
+
+hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
+
+gpu = st.selectbox('GPU',data['gpu'].unique())
+
+os = st.selectbox('OS',data['operatingsys'].unique())
+
+if st.button('Predict Price'):
+    query = np.array([company,type,ram,cpu,hdd,screen_size,hdd,gpu,os])
+
+    query = query.reshape(1,12)
+    st.title("The predicted price of this configuration is " + str(int(np.exp(pipe.predict(query)[0]))))
